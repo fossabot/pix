e1da29797a40a7a04bd9001254edaa6cbbfaefc8
LJA <ljacquemin@octo.com>
2019-05-13 11:55:50 +0200

    mon-pix: use currentUser in index
    Also, we no longer check for "board" users in index, and rely instead on /compte and /profilv2 to do that redirection if necessary.
    
    Likewise, we count on /compte to redirect to /login if necessary.

--- mon-pix/app/routes/index.js
+++ mon-pix/app/routes/index.js
@@ -1,25 +1,17 @@
-import { inject as service } from '@ember/service';
 import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
 
 export default Route.extend({
 
-  session: service(),
-  store: service(),
+  currentUser: service(),
 
-  beforeModel() {
-    if (this.get('session.isAuthenticated')) {
-      return this.store
-        .findRecord('user', this.get('session.data.authenticated.userId'))
-        .then((connectedUser) => {
-          if (connectedUser.get('organizations.length')) {
-            return this.transitionTo('board');
-          }
-          if (connectedUser.get('usesProfileV2')) {
-            return this.replaceWith('profilv2');
-          }
-          return this.transitionTo('compte');
-        });
+  async beforeModel() {
+    const usesProfileV2 = this.currentUser.get('user.usesProfileV2');
+    
+    if (usesProfileV2) {
+      return this.transitionTo('profilv2');
     }
-    return this.transitionTo('login');
+
+    return this.transitionTo('compte');
   },
 });
