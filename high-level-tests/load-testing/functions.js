a9f0724174d0e5af20dde467bc405e681997a4fc
Stephane Bedeau <sbedeau@octo.com>
2019-06-20 14:49:23 +0200

    Add new scenario to benchmark orga and api

--- high-level-tests/load-testing/functions.js
+++ high-level-tests/load-testing/functions.js
@@ -1,9 +1,32 @@
 const faker = require('faker');
 
 module.exports = {
+  foundNextChallenge,
+  getRandomCampaignId,
+  getRandomUsername,
   setupSignupFormData,
-  foundNextChallenge
 };
+
+function foundNextChallenge(context, next) {
+  const continueLooping = !!context.vars.challengeId;
+  return next(continueLooping);
+}
+
+function getRandomCampaignId(context, events, done) {
+  const campaigns = context.vars['campaigns'];
+  if (campaigns) {
+    const randomCampaign = campaigns[Math.floor(Math.random() * campaigns.length)];
+    context.vars['campaignId'] = randomCampaign.id;
+  }
+  return done();
+}
+
+function getRandomUsername(context, events, done) {
+  const userIds = [78181,138437, 29539, 29539, 55715, 29539, 21756, 17702, 51172, 21756, 31148, 21756, 21756, 21756, 5330, 5330, 21756, 31148, 155735, 31148, 21756, 21756, 31148, 28173, 253152, 192739, 151679, 221196, 31148, 60177]
+  const userId = userIds[Math.floor(Math.random() * userIds.length)];
+  context.vars['username'] = `${userId}@pix.fr`;
+  return done();
+}
 
 function setupSignupFormData(context, events, done) {
   context.vars['firstName'] = faker.name.firstName();
@@ -11,9 +34,4 @@ function setupSignupFormData(context, events, done) {
   context.vars['email'] = faker.internet.email();
   context.vars['password'] = 'L0rem1psum';
   return done();
-}
-
-function foundNextChallenge(context, next) {
-  const continueLooping = !!context.vars.challengeId;
-  return next(continueLooping);
 }
